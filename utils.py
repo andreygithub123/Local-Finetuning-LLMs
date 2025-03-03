6b14b26d3c1578b226d5509962ebeb74cc154d93
Andrei Diaconu <andreidiaconu.official@gmail.com>
2025-03-03 10:27:00 +0200

    modified hyperparameters for improved performance

--- utils.py
+++ utils.py
@@ -122,3 +122,16 @@ def generate_prime_numbers(n):
     return primes
 
 
+
+
+```python
+import random
+
+def generate_password(length):
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+,.?"'
+    password = ''.join(random.choice(chars) for _ in range(length))
+    return password
+
+if __name__ == "__main__":
+    print(generate_password(10))
+```

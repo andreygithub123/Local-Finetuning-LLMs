5963cffd696fa2272a0b4d1d0f139fff34c750ef
Andrei Diaconu <andreidiaconu.official@gmail.com>
2025-03-04 16:58:34 +0200

    tuned hyperparameters

--- utils.py
+++ utils.py
@@ -156,3 +156,16 @@ def generate_password(length: int, include_uppercase: bool, include_numbers: boo
     password = ""
     for _ in range(length):
         password += random.choice
+
+
+def generate_prime_numbers(n):
+    prime_numbers = []
+    for num in range(2, n+1):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            prime_numbers.append(num)
+    return prime_numbers
